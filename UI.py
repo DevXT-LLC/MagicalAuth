4d1c0be1caf8dc24bbe4c207160681e8d23a919e
Josh XT <josh@devxt.com>
2024-05-19 14:25:19 -0400

    Magic link send on register

--- UI.py
+++ UI.py
@@ -59,10 +59,6 @@ def login_page():
                     st.success(
                         "MFA token confirmed! Please check your email for the login link."
                     )
-                    magic_link = MagicalAuth(email=email).send_magic_link(
-                        otp=otp, ip_address=st.query_params.get("ip", [""])[0]
-                    )
-                    st.write(magic_link)
                     st.rerun()
                 else:
                     st.write("Invalid MFA token. Please try again.")
