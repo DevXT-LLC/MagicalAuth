7a4aac4cc68cdd16e25df901701e971ad9f9d46f
Josh XT <josh@devxt.com>
2024-06-13 13:02:19 -0400

    redirect to main page with delay

--- components/Auth.py
+++ components/Auth.py
@@ -338,8 +338,9 @@ def sso_redirect(provider: str):
         ):
             get_user()
     if "token" not in st.query_params and "code" not in st.query_params:
+        st.write("Redirecting to login page...")
         # Reload to ../ page
         st.markdown(
-            f'<meta http-equiv="refresh" content="0;URL=../">',
+            f'<meta http-equiv="refresh" content="1;URL=../">',
             unsafe_allow_html=True,
         )
