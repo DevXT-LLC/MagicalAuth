1408f53328ac3a492bb015192a2618d1ab641abb
Josh XT <josh@devxt.com>
2024-06-13 01:52:49 -0400

    more logging

--- components/Auth.py
+++ components/Auth.py
@@ -94,7 +94,7 @@ def google_sso_button():
                         st.session_state["oauth2_redirect_url"] = new_uri
                         st.session_state["oauth2_token_completed"] = True
                         st.session_state["oauth2_token_requested"] = False
-                        st.rerun()  # Rerun to apply the state change and redirect
+                        st.experimental_rerun()  # Rerun to apply the state change and redirect
                     else:
                         st.session_state["oauth2_token_requested"] = False
                         st.error("Unexpected response structure from backend.")
