b9e38eec95d48c2b11514bb7f2ba16b580f538b9
Josh XT <josh@devxt.com>
2024-06-13 12:46:30 -0400

    simplify

--- components/Auth.py
+++ components/Auth.py
@@ -299,3 +299,34 @@ def log_out_button():
             )
             time.sleep(2)
             st.stop()
+
+
+def sso_redirect(provider: str):
+    auth_uri = getenv("MAGICALAUTH_SERVER")
+    if "code" in st.query_params:
+        if (
+            st.query_params["code"] != ""
+            and st.query_params["code"] is not None
+            and st.query_params["code"] != "None"
+        ):
+            st.session_state["code"] = st.query_params["code"]
+    if "code" in st.session_state:
+        code = st.session_state["code"]
+        if code != "" and code is not None and code != "None":
+            referrer = f"{getenv('MAGIC_LINK_URL')}/{provider}"
+            response = requests.post(
+                f"{auth_uri}/v1/oauth2/{provider}",
+                json={"code": code, "referrer": referrer},
+            )
+            if response.status_code == 200:
+                data = response.json()
+                if "detail" in data:
+                    new_uri = data["detail"]
+                    st.markdown(
+                        f'<meta http-equiv="refresh" content="0;URL={new_uri}">',
+                        unsafe_allow_html=True,
+                    )
+                    st.stop()
+                else:
+                    st.error(data)
+                    st.stop()
--- pages/github.py
+++ pages/github.py
@@ -1,31 +1,3 @@
-import streamlit as st
-import requests
-from Globals import getenv
+from components.Auth import sso_redirect
 
-auth_uri = getenv("MAGICALAUTH_SERVER")
-if "code" in st.query_params:
-    if (
-        st.query_params["code"] != ""
-        and st.query_params["code"] is not None
-        and st.query_params["code"] != "None"
-    ):
-        st.session_state["code"] = st.query_params["code"]
-if "code" in st.session_state:
-    code = st.session_state["code"]
-    if code != "" and code is not None and code != "None":
-        response = requests.post(
-            f"{auth_uri}/v1/oauth2/github",
-            json={"code": code, "referrer": getenv("MAGIC_LINK_URL")},
-        )
-        if response.status_code == 200:
-            data = response.json()
-            if "detail" in data:
-                new_uri = data["detail"]
-                st.markdown(
-                    f'<meta http-equiv="refresh" content="0;URL={new_uri}">',
-                    unsafe_allow_html=True,
-                )
-                st.stop()
-            else:
-                st.error(data)
-                st.stop()
+sso_redirect(provider="github")
